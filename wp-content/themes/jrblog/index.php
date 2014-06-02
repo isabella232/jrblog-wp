1fb90484b5408b6500b122b37c68abdfc170c11a
David A Conway Jr <david@jrconway.net>
2014-06-01 21:16:59 -0500

    Added Gantry Updates

--- wp-content/themes/jrblog/index.php
+++ wp-content/themes/jrblog/index.php
@@ -1,17 +1,9 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * For example, it puts together the home page when no home.php file exists.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage jrConway.jrBlog
- * @since jrBlog 1.0
+ * @version   2.0.1 May 28, 2014
+ * @author    JaidynReiman http://www.jrconway.net
+ * @copyright Copyright (C) 2014 JaidynReiman
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
  */
 
 get_header(); ?>
